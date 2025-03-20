4828c248bbb585548de5183d01b9ed5bac4a1580
dulce.gonzalez <dulce.gonzalez@comsys.mx>
2025-03-19 21:20:34 -0700

    fix sessions

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -1,52 +1,41 @@
+// layout.tsx
 import type { Metadata } from "next";
-import { SessionProvider } from "next-auth/react";
-import localFont from "next/font/local";
-import { LazyMotion, domAnimation } from "framer-motion"
-import "./globals.css";
-import {
-  TooltipProvider,
-} from "@/components/ui/tooltip";
-import { Toaster } from "@/components/ui/toaster";
-import { auth } from "@/auth";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import { redirect } from "next/navigation";
+import Sidebar from "@/components/Sidebar";
+import { currentUser } from "@/lib/auth";
+import { ProtectedRouteWrapper } from "@/components/auth/ProtectedWrapper";
+import { cn } from "@/lib/utils";
+import { InicioCajaProvider } from "@/context/InicioCajaContext";
 
 export const metadata: Metadata = {
   title: "PRASE",
   description: "Sistema administrador de seguros",
 };
 
-export default async function RootLayout({
+export default async function ProtectedLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const session = await auth();
+  const userData = await currentUser();
+
+  if (!userData) {
+    redirect("/login");
+  }
 
   return (
-    <SessionProvider session={session}>
-      <html lang="es" suppressHydrationWarning>
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
-          <LazyMotion features={domAnimation}>
-            <TooltipProvider>
-              {children}
-            </TooltipProvider>
-            <Toaster />
-          </LazyMotion>
-          <div id="portal-root"></div>
-        </body>
-      </html>
-    </SessionProvider>
+    <InicioCajaProvider>
+      <div>
+        <Sidebar UsuarioID={userData.usuario.UsuarioID} NombreUsuario={userData.usuario.NombreUsuario} aplicaciones={userData?.aplicaciones || []} user={userData} />
+        <main className={cn(
+          "p-4 transition-all duration-300 ease-in-out min-h-screen",
+          "xl:p-8 xl:ml-64"
+        )}>
+          <ProtectedRouteWrapper aplicaciones={userData?.aplicaciones || []}>
+            {children}
+          </ProtectedRouteWrapper>
+        </main>
+      </div>
+    </InicioCajaProvider>
   );
 }
